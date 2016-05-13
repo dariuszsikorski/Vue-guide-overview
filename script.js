97506e20df05c0d444babe0253fabebec51b6fe8
Dariusz Sikorski <dariuszsikorski.pl@gmail.com>
2016-05-13 15:56:11 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+$(function(){
+  $(".index > ul > li > .link").stick_in_parent();
+  $(".index > ul > li > ul > li > .link").stick_in_parent({
+    offset_top: 38
+  });
+  // $("").stick_in_parent();
+});
